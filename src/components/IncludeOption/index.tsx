52dc551d204ba89727eeef84952ad04b7a511184
Esther Ferreira <estherferreira.cm@gmail.com>
2023-05-21 00:44:50 -0300

    feat: change text value

--- src/components/IncludeOption/index.tsx
+++ src/components/IncludeOption/index.tsx
@@ -10,19 +10,19 @@ export function IncludeOption() {
       <div className={style.leftSide}>
         <TitleAndDescription
           title="Como está o ar hoje?"
-          text="Me ajude com algumas informações!"
+          text="Preencha os valores para cada parâmetro e verifique como está o ar"
         ></TitleAndDescription>
         <Button text="Enviar dados"></Button>
       </div>
 
           <div className={style.rightSide}>
-            <div className="flex flex-col gap-[1rem]">
+            <div className="flex flex-col gap-[2rem]">
               <FloatingLabel text="MP10"></FloatingLabel>
               <FloatingLabel text="MP2.5"></FloatingLabel>
               <FloatingLabel text="O3"></FloatingLabel>
             </div>
 
-            <div className="flex flex-col gap-[1rem]">
+            <div className="flex flex-col gap-[2rem]">
               <FloatingLabel text="CO"></FloatingLabel>
               <FloatingLabel text="NO2"></FloatingLabel>
               <FloatingLabel text="SO2"></FloatingLabel>
