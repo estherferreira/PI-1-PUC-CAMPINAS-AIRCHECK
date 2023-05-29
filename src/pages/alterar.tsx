3ca9e3f7f99ef9fea8efe6a5e5ce05564c3e41f2
Esther Ferreira <estherferreira.cm@gmail.com>
2023-05-29 14:30:12 -0300

    feat: create "alterar" page

---
+++ src/pages/alterar.tsx
@@ -0,0 +1,163 @@
+import style from "../styles/alterar.module.css";
+import Image from "next/image";
+
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableContainer,
+} from "@chakra-ui/react";
+import { IconWithText } from "@/components/IconWithText";
+
+import Logo from "../../public/Logo.svg";
+import Pen from "../../public/pen.svg";
+import Delete from "../../public/delete.svg";
+import Logout from "../../public/logout.svg";
+
+export default function Alterar() {
+  return (
+    <main className={style.main}>
+      <div className="flex justify-between">
+        <Image src={Logo} alt="Logo"></Image>
+        <div className="flex gap-[4.1875rem]">
+          <IconWithText
+            href="/alterar"
+            src={Pen}
+            alt="Pen"
+            text="Alterar"
+          ></IconWithText>
+          <IconWithText
+            href="/"
+            src={Delete}
+            alt="Delete"
+            text="Excluir"
+          ></IconWithText>
+          <IconWithText
+            href="/sair"
+            src={Logout}
+            alt="Logout"
+            text="Sair"
+          ></IconWithText>
+        </div>
+      </div>
+      <div className={style.content}>
+        <TableContainer overflowX={"hidden"}>
+          <Table>
+            <Thead>
+              <Tr>
+                <Th
+                  style={{
+                    color: "#FFF",
+                    fontFamily: "Inter, sans-serif",
+                    fontSize: "14px",
+                    fontWeight: "500",
+                    paddingRight: "8.875rem",
+                    paddingBottom: "1rem",
+                  }}
+                >
+                  ID
+                </Th>
+                <Th
+                  style={{
+                    color: "#FFF",
+                    fontFamily: "Inter, sans-serif",
+                    fontSize: "14px",
+                    fontWeight: "500",
+                    paddingRight: "8.875rem",
+                    paddingBottom: "1rem",
+                  }}
+                >
+                  MP10
+                </Th>
+                <Th
+                  style={{
+                    color: "#FFF",
+                    fontFamily: "Inter, sans-serif",
+                    fontSize: "14px",
+                    fontWeight: "500",
+                    paddingRight: "8.875rem",
+                    paddingBottom: "1rem",
+                  }}
+                >
+                  MP25
+                </Th>
+                <Th
+                  style={{
+                    color: "#FFF",
+                    fontFamily: "Inter, sans-serif",
+                    fontSize: "14px",
+                    fontWeight: "500",
+                    paddingRight: "8.875rem",
+                    paddingBottom: "1rem",
+                  }}
+                >
+                  O3
+                </Th>
+                <Th
+                  style={{
+                    color: "#FFF",
+                    fontFamily: "Inter, sans-serif",
+                    fontSize: "14px",
+                    fontWeight: "500",
+                    paddingRight: "8.875rem",
+                    paddingBottom: "1rem",
+                  }}
+                >
+                  CO
+                </Th>
+                <Th
+                  style={{
+                    color: "#FFF",
+                    fontFamily: "Inter, sans-serif",
+                    fontSize: "14px",
+                    fontWeight: "500",
+                    paddingRight: "8.875rem",
+                    paddingBottom: "1rem",
+                  }}
+                >
+                  NO2
+                </Th>
+                <Th
+                  style={{
+                    color: "#FFF",
+                    fontFamily: "Inter, sans-serif",
+                    fontSize: "14px",
+                    fontWeight: "500",
+                    paddingRight: "8.875rem",
+                    paddingBottom: "1rem",
+                  }}
+                >
+                  SO2
+                </Th>
+                <Th
+                  style={{
+                    paddingRight: "8.875rem",
+                    paddingBottom: "1rem",
+                  }}
+                ></Th>
+              </Tr>
+            </Thead>
+
+            <Tbody>
+              <Tr>
+                <Td>10</Td>
+                <Td>10</Td>
+                <Td>10</Td>
+                <Td>10</Td>
+                <Td>10</Td>
+                <Td>10</Td>
+                <Td>10</Td>
+                <Td>
+                  <Image src={Pen} alt="Alterar Amostra"></Image>
+                </Td>
+              </Tr>
+            </Tbody>
+          </Table>
+        </TableContainer>
+      </div>
+    </main>
+  );
+}
