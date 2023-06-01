1ee98297fbd33799d61aecd8493be12f81352f7f
Esther Ferreira <estherferreira.cm@gmail.com>
2023-06-01 16:25:07 -0300

    refactor: Adapt colors to another background color

--- src/components/IconWithText/index.tsx
+++ src/components/IconWithText/index.tsx
@@ -1,18 +1,35 @@
-import Image from 'next/image'
-import Link from 'next/link';
-import style from './iconWithText.module.css'
+import Image from "next/image";
+import Link from "next/link";
+import style from "./iconWithText.module.css";
 
 interface IconWithTextProps {
-    src: any;
-    alt: string;
-    text: string;
-    href: string
+  src: any;
+  alt: string;
+  text: string;
+  href: string;
+  backgroundColor?: string;
 }
 
-export function IconWithText({ src, alt, text, href }: IconWithTextProps) {
-    return (<Link href={href}>
-        <div className={style.container}>
-            <Image src={src} alt={alt}></Image>
-            <span className={style.span}>{text}</span>
-        </div></Link>);
+export function IconWithText({
+  src,
+  alt,
+  text,
+  href,
+  backgroundColor,
+}: IconWithTextProps) {
+  const textColorClassName =
+    backgroundColor === "#fafafa" ? style.black : style.span;
+
+  return (
+    <Link href={href}>
+      <div className={style.container}>
+        <Image
+          className={backgroundColor === "#fafafa" ? style.blackIcon : style.span}
+          src={src}
+          alt={alt}
+        />
+        <p className={textColorClassName}>{text}</p>
+      </div>
+    </Link>
+  );
 }
--- src/pages/alterar.tsx
+++ src/pages/alterar.tsx
@@ -2,29 +2,20 @@ import style from "../styles/alterar.module.css";
 import Image from "next/image";
 import Link from "next/link";
 
-import {
-  Table,
-  Thead,
-  Tbody,
-  Tr,
-  Th,
-  Td,
-  TableContainer,
-} from "@chakra-ui/react";
 import { IconWithText } from "@/components/IconWithText";
+import { EditableTable } from "@/components/EditableTable/editableTable";
 
-import Logo from "../../public/Logo.svg";
 import Pen from "../../public/pen.svg";
 import Delete from "../../public/delete.svg";
 import Logout from "../../public/logout.svg";
-import { EditableTable } from "@/components/EditableTable";
 
 export default function Alterar() {
+  const backgroundColor = '#fafafa';
   return (
     <main className={style.main}>
       <div className="flex justify-between">
         <Link href={"/"}>
-          <Image src={Logo} alt="Logo"></Image>
+          <Image src="/black-logo.png" alt="Logo" width={169.895} height={28.0696}></Image>
         </Link>
         <div className="flex gap-[4.1875rem]">
           <IconWithText
@@ -32,138 +23,18 @@ export default function Alterar() {
             src={Pen}
             alt="Pen"
             text="Alterar"
-          ></IconWithText>
-          <IconWithText
-            href="/"
-            src={Delete}
-            alt="Delete"
-            text="Excluir"
+            backgroundColor={backgroundColor}
           ></IconWithText>
           <IconWithText
             href="/sair"
             src={Logout}
             alt="Logout"
             text="Sair"
+            backgroundColor={backgroundColor}
           ></IconWithText>
         </div>
       </div>
       <EditableTable></EditableTable>
-      <div className={style.content}>
-        {/* <TableContainer overflowX={"hidden"}>
-          <Table>
-            <Thead>
-              <Tr>
-                <Th
-                  style={{
-                    color: "#FFF",
-                    fontFamily: "Inter, sans-serif",
-                    fontSize: "14px",
-                    fontWeight: "500",
-                    paddingRight: "8.875rem",
-                    paddingBottom: "1rem",
-                  }}
-                >
-                  ID
-                </Th>
-                <Th
-                  style={{
-                    color: "#FFF",
-                    fontFamily: "Inter, sans-serif",
-                    fontSize: "14px",
-                    fontWeight: "500",
-                    paddingRight: "8.875rem",
-                    paddingBottom: "1rem",
-                  }}
-                >
-                  MP10
-                </Th>
-                <Th
-                  style={{
-                    color: "#FFF",
-                    fontFamily: "Inter, sans-serif",
-                    fontSize: "14px",
-                    fontWeight: "500",
-                    paddingRight: "8.875rem",
-                    paddingBottom: "1rem",
-                  }}
-                >
-                  MP25
-                </Th>
-                <Th
-                  style={{
-                    color: "#FFF",
-                    fontFamily: "Inter, sans-serif",
-                    fontSize: "14px",
-                    fontWeight: "500",
-                    paddingRight: "8.875rem",
-                    paddingBottom: "1rem",
-                  }}
-                >
-                  O3
-                </Th>
-                <Th
-                  style={{
-                    color: "#FFF",
-                    fontFamily: "Inter, sans-serif",
-                    fontSize: "14px",
-                    fontWeight: "500",
-                    paddingRight: "8.875rem",
-                    paddingBottom: "1rem",
-                  }}
-                >
-                  CO
-                </Th>
-                <Th
-                  style={{
-                    color: "#FFF",
-                    fontFamily: "Inter, sans-serif",
-                    fontSize: "14px",
-                    fontWeight: "500",
-                    paddingRight: "8.875rem",
-                    paddingBottom: "1rem",
-                  }}
-                >
-                  NO2
-                </Th>
-                <Th
-                  style={{
-                    color: "#FFF",
-                    fontFamily: "Inter, sans-serif",
-                    fontSize: "14px",
-                    fontWeight: "500",
-                    paddingRight: "8.875rem",
-                    paddingBottom: "1rem",
-                  }}
-                >
-                  SO2
-                </Th>
-                <Th
-                  style={{
-                    paddingRight: "8.875rem",
-                    paddingBottom: "1rem",
-                  }}
-                ></Th>
-              </Tr>
-            </Thead>
-
-            <Tbody>
-              <Tr>
-                <Td>10</Td>
-                <Td>10</Td>
-                <Td>10</Td>
-                <Td>10</Td>
-                <Td>10</Td>
-                <Td>10</Td>
-                <Td>10</Td>
-                <Td>
-                  <Image src={Pen} alt="Alterar Amostra"></Image>
-                </Td>
-              </Tr>
-            </Tbody>
-          </Table>
-        </TableContainer> */}
-        
-      </div>
     </main>
   );
 }
